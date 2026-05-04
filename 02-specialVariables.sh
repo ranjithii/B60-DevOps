e08b90c36bc38047307ddfb75b1695f1ee73188e
ranjithii <indranstars@gmail.com>
2026-05-04 15:58:38 -0400

    update

---
+++ 02-specialVariables.sh
@@ -0,0 +1,11 @@
+firstVar=$1
+secondVar=$2
+thirdVar=$3
+
+echo "I am printing the first argument $firstVar"
+echo "I am printing the second argument $secondVar"
+
+echo "Name of the executed script is $0"
+
+echo "Arguments used in the script are $#"
+echo "Arguments used in the script are $@"
