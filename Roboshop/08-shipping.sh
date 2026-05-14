31fcbcb044e3440fd0884a85fd11edc270cd2cb0
ranjithii <indranstars@gmail.com>
2026-05-14 18:52:56 -0400

    Create 08-shipping.sh

---
+++ Roboshop/08-shipping.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# I want to make sure that the scirpt has to validate whether the user running the script is root user or not, if not root user, script has to be exited
+COMPONENT="shipping"
+source ./common.sh
+maven
